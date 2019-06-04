2e6ff880c610dd1d594d65c50089f5e53fbf9944
ronak <parkhiya.r@northeastern.edu>
2019-06-03 22:41:09 -0400

    Adding validation to book url

--- webapp/routes/books.js
+++ webapp/routes/books.js
@@ -19,22 +19,52 @@ router.post('/',authorization.checkAccess ,function(req, res, next) {
 
 router.get('/', authorization.checkAccess, function (req,res, next) {
     sql.query(sqlStatement.getAllBookSQL(), function (err, result, fields) {
-        if (err) throw err;
-        res.status(200).json(result);
+        if (err) res.status(500).json({
+            message:"SQL error",
+            error: err
+        });
+        else {
+            if (result[0] == null)
+                res.status(204).json(result);
+            else
+                res.status(200).json(result);
+        }
     })
 
 })
 
-router.put('/:bookID',authorization.checkAccess ,function(req, res, next) {
-    let bookID = req.params.bookID;
+router.put('/',authorization.checkAccess ,function(req, res, next) {
+    let bookID = req.body.id;
     let title = req.body.title;
     let author = req.body.author;
     let isbn = req.body.isbn;
     let quantity = req.body.quantity;
-    sql.query(sqlStatement.getupdateBookSQL(bookID,title,author,isbn,quantity), function (err, result, fields) {
-        if (err) throw err;
-        res.status(200).json(result);
-    })
+    if (bookID != null && title != null && author != null && isbn != null && quantity >= 0) {
+        sql.query(sqlStatement.getupdateBookSQL(bookID, title, author, isbn, quantity), function (err, result, fields) {
+            if (err) {
+                res.status(500).json({
+                    message: "SQL error"
+                });
+            }else {
+                if(result.changedRows!=0){
+                    res.status(200).json({
+                        message:"Updated Successfully",
+                        data: result
+                    });
+                }else{
+                    res.status(204).json({
+                        message:"No content available foterr entered book id"
+                    });
+                }
+
+            }
+        })
+    }
+    else{
+        res.status(400).json({
+            message: "Bad Request: Please enter all details"
+        })
+    }
 
 })
 
