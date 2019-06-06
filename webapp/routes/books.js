0ad835ecdc9e2c47f7c7ffc0a177353a2684010a
Mayank Barua <barua.m@husky.neu.edu>
2019-06-06 01:07:01 -0400

    Adding post method of book changes to repo

--- webapp/routes/books.js
+++ webapp/routes/books.js
@@ -3,23 +3,31 @@ const router = express.Router();
 const sql = require('../db.js');
 const SQL = require('../Service/sql.js')
 const authorization = require('../Service/authorization');
+const uuidv4 = require('uuid/v4');
 
 const sqlStatement = new SQL();
 
 router.post('/',authorization.checkAccess ,function(req, res, next) {
+    let id = uuidv4();
     let title = req.body.title;
     let author = req.body.author;
     let isbn = req.body.isbn;
     let quantity = req.body.quantity;
     if(title != null && author != null && isbn !=null && quantity>=1){
-    sql.query(sqlStatement.getAddBookSQL(title,author,isbn,quantity), function (err, result, fields) {
-        if (err)
-            res.status(500).json({
-                messgae:"SQL error"
-            });
-        else
-            res.status(201).json(result);
-    });}else{
+        sql.query(sqlStatement.getAddBookSQL(id,title,author,isbn,quantity), function (err, result, fields) {
+            if (err)
+                res.status(500).json({
+                    messgae:"SQL error"
+                });
+            else
+                res.status(201).json({
+                    id:id,
+                    title: title,
+                    author: author,
+                    isbn: isbn,
+                    quantity: quantity
+                });
+        });}else{
         res.status(400).json({
             messgae:"Bad Request : Please enter all details"
         });
