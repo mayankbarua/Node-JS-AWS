bbcdc74ed728a8e70f7cc9db3aaa4536b93c72f1
Mayank Barua <barua.m@husky.neu.edu>
2019-06-05 23:03:23 -0400

    Adding test case features

--- webapp/app.js
+++ webapp/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var usersRouter = require('./routes/users');
-var booksRouter = require('./routes/books')
+var booksRouter = require('./routes/books');
 
 var app = express();
 
