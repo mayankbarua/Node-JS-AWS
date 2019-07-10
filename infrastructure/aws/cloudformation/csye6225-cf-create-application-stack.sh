f35034bdbdaedcba99eab1797a2426f1a4167edc
jaychitalia95 <chitalia.j@husky.neu.edu>
2019-07-09 21:15:44 -0400

    Adding cloudformation changes (policies, roles and codedeploy application/group)

--- infrastructure/aws/cloudformation/csye6225-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-cf-create-application-stack.sh
@@ -4,13 +4,17 @@
 STACK_NAME=$1
 AMI_ID=$2
 NETWORKING_STACK_NAME=$3
+CODEDEPLOY_S3=$4
+IMAGE_S3=$5
 
-if [[ -z "$1" || -z "$2" || -z "$3" ]]
+
+
+if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" || -z "$5" ]]
 then
-	echo "Please enter all parameters in order ( Stack Name, AMI id, Networking Stack Name )"
+	echo "Please enter all parameters in order ( Stack Name, AMI id, Networking Stack Name, CodeDeploy S3 Bucket, Image store s3 Bucket )"
 	exit 1
 else
-	aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-application.json --parameters ParameterKey=AMIid,ParameterValue=$AMI_ID ParameterKey=NetworkStackName,ParameterValue=$NETWORKING_STACK_NAME --capabilities CAPABILITY_NAMED_IAM
+	aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-application.json --parameters ParameterKey=AMIid,ParameterValue=$AMI_ID ParameterKey=NetworkStackName,ParameterValue=$NETWORKING_STACK_NAME ParameterKey=CodeDeployS3Bucket,ParameterValue=$CODEDEPLOY_S3 ParameterKey=ImageS3Bucket,ParameterValue=$IMAGE_S3 --capabilities CAPABILITY_NAMED_IAM
     if [ $? -eq 0 ]; then
         aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
         if [ $? -eq 0 ]; then
