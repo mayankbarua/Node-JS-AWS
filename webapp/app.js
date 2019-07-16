70b1eba3c20b0d510b600807a502bd9ecb7f4179
ronak <parkhiya.r@northeastern.edu>
2019-07-15 20:37:34 -0400

    apiend point changed

--- webapp/app.js
+++ webapp/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', usersRouter);
-app.use('/books',booksRouter);
+app.use('/booksronak',booksRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
