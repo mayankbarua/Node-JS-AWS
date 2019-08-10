dceaa0696fb2def99ac46d5d9f54661d71bf0754
Mayank Barua <barua.m@husky.neu.edu>
2019-08-10 12:18:27 -0400

    Check file in ec2 changes

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cd home/
+FILE=checkFile.sh
+while ! test -f "$FILE"; do
+    sleep 30s
+    echo "$FILE exist"
+done
+
 cd home/centos/webapp
 pwd
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/cloudwatch-agent-config.json -s
--- infrastructure/aws/codedeploy/applicationStop.sh
+++ infrastructure/aws/codedeploy/applicationStop.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sleep 2m
 runuser -l centos -c 'pm2 stop API'
